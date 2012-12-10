d0c379ae851bec9d392f7f4e5dd36702d01908f8
Anatoli Steinhauer <stan1306@gmx.de>
2012-12-10 22:45:51 +0100

    Changes on PlayWin

--- src/gui/PlayWin.java
+++ src/gui/PlayWin.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,6 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.text.StyledEditorKit.FontSizeAction;
 
 public class PlayWin {
 	
@@ -22,7 +24,7 @@ public class PlayWin {
 		frame.setLayout(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//frame.setSize(400,500);
-		frame.setBounds(400,225 , 400, 400);
+		frame.setBounds(400, 225 , 400, 350);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		
@@ -43,12 +45,15 @@ public class PlayWin {
 		JLabel label = new JLabel();
 		label.setLocation(90, 90);
 		label.setSize(380, 20);
-		label.setBounds(100, 250, 380, 20);
+		label.setBounds(45, 250, 380, 30);
 		label.setText("Glückwunsch! Sie haben gewonnen!");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Dialog", 0, 18));
 		
 		
-		JButton btn = new JButton("OK");
-		btn.setBounds(165, 300, 75, 75);
+		
+	/*	JButton btn = new JButton("OK");
+		btn.setBounds(30, 90, 7, 75);
 		btn.setBorder(null);
 		
 		btn.addActionListener(new ActionListener(){
@@ -59,11 +64,37 @@ public class PlayWin {
 				frame.dispose();
 					
 			}
+		});*/
+		
+		JButton laden = new JButton("Laden");
+		laden.setBounds(0, 290, 200, 30);
+		laden.setBorder(null);
+		
+		laden.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent arg0)
+			{						
+				frame.dispose();
+				LoadDialog ld = new LoadDialog();
+			}
+		});
+		
+		JButton ende = new JButton("Weiter");
+		ende.setBounds(200, 290, 200, 30);
+		ende.setBorder(null);
+		
+		ende.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent arg0)
+			{						
+				frame.dispose();				
+			}
 		});
 		
 		frame.getContentPane().add(label);
 		frame.getContentPane().add(background);
-		frame.getContentPane().add(btn);
+		frame.getContentPane().add(laden);
+		frame.getContentPane().add(ende);
 		frame.repaint();
 		
 	}
