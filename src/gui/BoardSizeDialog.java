a7242438c0107da597c6d991cb001229f6eb53d3
Mats Shok <mats.bulin@web.de>
2012-12-10 11:45:04 +0100

    comments pattern

--- src/gui/BoardSizeDialog.java
+++ src/gui/BoardSizeDialog.java
@@ -14,7 +14,8 @@ import javax.swing.border.Border;
  * @version 0.1
  *
  */
-public class BoardSizeDialog extends JFrame{
+
+public class BoardSizeDialog extends JFrame{ 
 
 	private
 
@@ -22,8 +23,8 @@ public class BoardSizeDialog extends JFrame{
 	int spalte;
 
 	public BoardSizeDialog(final PlayFrame playFrame){
-
-		final JFrame frame = new JFrame("Spielfeldgröße");
+		
+		final JFrame frame = new JFrame("Spielfeldgröße"); // Erstellung Frame
 		frame.setLayout(null);
 		frame.setSize(230, 150);
 		frame.setVisible(true);
@@ -35,35 +36,36 @@ public class BoardSizeDialog extends JFrame{
 		background.setOpaque(false);
 		background.setBounds(0,0,230,150);
 
+		// Abfrage Spielfeldgroesse (an Benutzer)
 		JLabel label = new JLabel();
 		label.setText("<html><p/><p/>Geben Sie die Spielfeldgröße an: <p/><p/></html>");
 		label.setForeground(Color.WHITE);
 		label.setBounds(20, 5, 280, 25);
 
-		JLabel x = new JLabel();
-		x.setText("<html><p/><p/>Zeilen: <p/><p/></html>");
+		JLabel x = new JLabel(); // Zeilen
+		x.setText("<html><p/><p/>Zeilen: <p/><p/></html>"); 
 		x.setForeground(Color.WHITE);
 		x.setBounds(25, 40, 50, 25);
-
-		NumberFormat format = NumberFormat.getInstance();
-
+		
+		NumberFormat format = NumberFormat.getInstance(); 
+		
 		final JFormattedTextField rows = new JFormattedTextField(format);
-		((javax.swing.text.NumberFormatter)rows.getFormatter()).setAllowsInvalid(false);
-		rows.setText("10");
+		((javax.swing.text.NumberFormatter)rows.getFormatter()).setAllowsInvalid(false); // Abfangen falscher Eingaben (nur Zahlen)
+		rows.setText("10"); // Standarttext auf "10"
 		rows.setBounds(70, 40, 20, 20);
 
-		JLabel y = new JLabel();
+		JLabel y = new JLabel(); // Spalten
 		y.setText("<html><p/><p/>Spalten: <p/><p/></html>");
 		y.setForeground(Color.WHITE);
 		y.setBounds(120, 40, 50, 25);
 
 		final JFormattedTextField cols = new JFormattedTextField(format);
-		((javax.swing.text.NumberFormatter)cols.getFormatter()).setAllowsInvalid(false);
-		cols.setText("10");
+		((javax.swing.text.NumberFormatter)cols.getFormatter()).setAllowsInvalid(false); // Abfangen falscher Eingaben (nur Zahlen)
+		cols.setText("10"); // Standarttext auf "10"
 		cols.setBounds(175, 40, 20, 20);
 		
 
-		JButton ok = new JButton("OK");
+		JButton ok = new JButton("OK"); // Button "OK"
 		ok.setBounds(80, 80, 70, 30);
 		ok.setBorder(null);
 
@@ -71,9 +73,10 @@ public class BoardSizeDialog extends JFrame{
 
 			public void actionPerformed(ActionEvent arg0)
 			{	
-				zeile = new Integer(rows.getText());
-				spalte = new Integer(cols.getText());
-				if (zeile < 5 || zeile > 50 ||spalte < 5|| spalte > 50){
+				zeile = new Integer(rows.getText()); // einlesen Reihen
+				spalte = new Integer(cols.getText()); // einlesen Spalten
+				
+				if (zeile < 5 || zeile > 50 ||spalte < 5|| spalte > 50){ // Feldgrößen MIN und MAX Abfrage
 					JOptionPane pane = new JOptionPane("Die Feldgröße muss zwischen 5 x 5 und 50 x 50 liegen!! ", JOptionPane.ERROR_MESSAGE);
 					JDialog dialog = pane.createDialog("SternenHimmelPuzzle");
 					dialog.setAlwaysOnTop(true);
@@ -82,7 +85,7 @@ public class BoardSizeDialog extends JFrame{
 				else
 				{
 					if(playFrame._oBoard instanceof GuiElementGameBoard){
-						playFrame.drawGameBoard(zeile,spalte);						
+						playFrame.drawGameBoard(zeile,spalte); // Spielfeld erstellen in Groesse (zeile, spalte)						
 					}
 					else
 						playFrame.drawEditorBoard(zeile,spalte);					
@@ -91,7 +94,7 @@ public class BoardSizeDialog extends JFrame{
 			}
 		});
 
-
+		// Frame adds
 		frame.getContentPane().add(label);
 		frame.getContentPane().add(rows);
 		frame.getContentPane().add(x);
