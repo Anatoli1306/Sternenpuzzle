5c294132f23160994521d1b168708f42aabb3dde
Mats Shok <mats.bulin@web.de>
2012-12-12 08:49:46 +0100

    icon in instruction dialog - small change

--- src/gui/InstructionDialog.java
+++ src/gui/InstructionDialog.java
@@ -2,7 +2,10 @@ package gui;
 
 
 import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,33 +22,11 @@ import javax.swing.JLabel;
 public class InstructionDialog {
 
 	JFrame frame;
-	//	JLabel label;
 	JLabel backGroundImage;
 
 	public InstructionDialog() {
 		frame = new JFrame ("Sternenpuzzle - Info");
-		//	        label = new JLabel ("");
-		//	        label.setHorizontalAlignment(JLabel.CENTER);
-		//	        label.setVerticalAlignment(JLabel.TOP);
-		//	        label.setForeground(Color.white);
-		//	        label.setText("<html><p/><p/><h1>Anleitung: </h1><p/><p/>" +
-		//	        		"Zeichnen Sie die Sterne ein. <p/>"+
-		//	        		"Auf jeden Stern muss mindestens ein Pfeil zeigen, <p/>" +
-		//	        		"und jeder Pfeil muss mindestens auf einen Stern zeigen. <p/>" +
-		//	        		"Pfeile zeigen auf die ganze Reihe, Spalte oder Diagonale; <p/>" +
-		//	        		"auch über andere Pfeile und Sterne hinweg <p/>" +
-		//	        		"Die Zahlen am Rand des Diagrams nennen die Anzahl der Sterne <p/>" +
-		//	        		"in der entsprechenden Zeile, bezeihungsweise Spalte. <p/> <p/>" +
-		//	        		"<h1>Tipp: </h1> <p/> <p/>" +
-		//	        		"Finden Sie zuerst die Fehler, die von keinem Pfeil angezeigt werden. <p/>" +
-		//	        		"Makieren Sie diese als Leerfelder. <p/>" +
-		//	        		"Alle Zeilen und Spalten mit einer 1 und einem Pfeil, <p/>" +
-		//	        		"der in die betreffende Reihe zeigt, können hinter dem Pfeil <p/>"+
-		//	        		"mit Leerfeldern gekennzeichnet werden. <p/>"+
-		//	        		"Schauen Sie sich jetzt die hohen Zahlen an. <p/>" +
-		//	        		"Sind in der jeweiligen Reihe nur noch so viele Felder wie angegeben? <p/>" +
-		//	        		"Suchen Sie am besten dort nach Sternen!");	        
-		//	        frame.add(label);
+
 		frame.setSize(472, 622);
 		frame.setVisible(true);
 		frame.setResizable(false);
@@ -56,5 +37,14 @@ public class InstructionDialog {
 		backGroundImage.setOpaque(false);
 		frame.getContentPane().add(backGroundImage);
 		backGroundImage.setBounds(0,0,472,622);
+		
+		// Icon setzen
+	    BufferedImage image = null;
+		try {
+			image = ImageIO.read(this.getClass().getResource("/resources/msIcon.ico"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    frame.setIconImage(image);
 	}
 }
